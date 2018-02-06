6205a35942fbaec53bcec9f91dee79a0c79bd8c7
Mac <mac@Macs-MacBook.local>
2018-02-06 12:26:38 +0300

    .

---
+++ treasure2/Assets/ConstructionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructionController : MonoBehaviour {
+
+
+	private Camera playerCamera;
+	private GameObject itemObject;
+	private float distance = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		playerCamera = GetComponentInChildren<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( itemObject != null ) {
+			itemObject.transform.position = playerCamera.transform.position + playerCamera.transform.forward * distance;
+			itemObject.transform.rotation = new Quaternion( 0.0f, playerCamera.transform.rotation.y, 0.0f, playerCamera.transform.rotation.w );
+		}
+	}
+}
---
+++ treasure2/Assets/treasureOpen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treasureOpen : MonoBehaviour {
+	private Animation ani;
+	// Use this for initialization
+	void Start () {
+		ani = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void open(){
+
+		if (!ani.isPlaying) {
+			ani.Play ();
+		} else {
+		
+			ani.Stop ();
+		}
+	}
+}
